d39c73c09f98c780202a89bff0e294a7b82ab589
Ocarinna <51068885+Ocarinna@users.noreply.github.com>
2025-05-06 13:56:48 -0300

    Add files via upload

---
+++ 01_hello_world/01_setting_up.py
@@ -0,0 +1,5 @@
+#Copy and paste this line of code in line 3:
+
+#print('Hi')
+
+print('Hi')
---
+++ 01_hello_world/05_snail_mail.py
@@ -0,0 +1,22 @@
+#Create a blank letter.py program.
+
+#In this exercise, you will write a letter to your future self... in Python.
+
+#Take a moment and think about what you hope to achieve on this journey.
+
+#Use print() to output:
+
+#Today's date.
+#How you are feeling right now.
+#What you want to accomplish by learning to code.
+#A little message to your older, wiser, and programmer self.
+#Your favorite emoji to spice things up!
+#When it's done, take a screenshot and click the Twitter icon to post your letter!
+
+#P.S. We will return the letter when the time is right.
+
+print('23/04/2025')
+print('cansado')
+print('trabalhar')
+print('não pare de estudar, preguiçoso')
+print('😊')
